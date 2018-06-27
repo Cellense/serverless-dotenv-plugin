8a518c4763306b9b321da780b1a9f35c6695aa3f
unknown <matus.congrady@buffpanel.com>
2018-06-27 15:29:18 +0200

    adjust loading .env files for buffpanel needs

--- index.js
+++ index.js
@@ -1,59 +1,37 @@
-'use strict';
-
 const fs = require('fs');
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
-const chalk = require('chalk');
+const path = require('path');
 
 class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.env = {};
     this.serverless.service.provider.environment = this.serverless.service.provider.environment || {};
-    this.hooks = {
-      "before:offline:start:init": this.loadEnv.bind(this),
-      "before:offline:start": this.loadEnv.bind(this),
-      "before:invoke:local:invoke": this.loadEnv.bind(this),
-      "before:deploy:resources": this.loadEnv.bind(this),
-      "before:deploy:functions": this.loadEnv.bind(this),
-      "before:package:initialize": this.loadEnv.bind(this),
-    }
+    this.loadEnv();
   }
 
   loadEnv() {
     try {
       this.serverless.cli.log('DOTENV: Loading environment variables:');
-      var config = this.serverless.service.custom['dotenv'];
-      var envPath = (config && config.path) || '.env';
-      this.env = dotenvExpand(dotenv.config({path: envPath})).parsed;
-      if (!this.env) {
+      const stage = this.serverless.processedInput.options.stage
+      const dotenvPath = stage ? `.env.${stage}` : '.env';
+      const envVariables = dotenvExpand(dotenv.config({ path: path.resolve(process.cwd(), dotenvPath) })).parsed;
+      if (!envVariables) {
         throw new this.serverless.classes.Error('[serverless-dotenv-plugin] Could not find .env file.');
         return false;
       }
 
-      var include = false;
-      if (config && config.include) {
-        include = config.include;
-      }
-      if (include) {
-        Object.keys(this.env)
-          .filter((key) => !include.includes(key))
-          .forEach((key) => {
-            delete this.env[key]
-          })
-      }
       Object.keys(this.env)
         .forEach((key) => {
           this.serverless.cli.log("\t - " + key);
-          this.serverless.service.provider.environment[key] = this.env[key];
+          this.serverless.service.provider.environment[key] = env[key];
         })
     } catch (e) {
-        console.error(chalk.red('\n Serverless Plugin Error --------------------------------------\n'))
-        console.error(chalk.red('  ' + e.message));
+        console.error('\n Serverless Plugin Error --------------------------------------\n')
+        console.error(`   ${e.message}`);
     }
-
   }
-
 }
 
 module.exports = ServerlessPlugin;
