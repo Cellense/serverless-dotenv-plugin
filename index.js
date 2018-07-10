07b49e5c2ec302d86c9360b352f8223bc79b2447
unknown <matus.congrady@buffpanel.com>
2018-07-10 12:00:19 +0200

    Adjusted loading .env files based on stage option

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-const fs = require('fs');
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
+const { success, fatal } = require('signale');
 const path = require('path');
 
-class ServerlessPlugin {
-  constructor(serverless, options) {
-    this.serverless = serverless;
-    this.env = {};
-    this.serverless.service.provider.environment = this.serverless.service.provider.environment || {};
-    this.loadEnv();
+function loadEnv(stage) {
+  try {
+    const dotenvPath = (stage === 'production' || stage === 'staging') ? '.env.dev' : `.env.${stage}`;
+    const envVariables = dotenvExpand(dotenv.config({ path: path.resolve(process.cwd(), dotenvPath) })).parsed;
+
+    success(`Loading env variables from ${dotenvPath}`);
+    Object.entries(envVariables).forEach(([name, value]) => {
+      success(`${name}: ${value}`)
+    });
+  } catch (e) {
+    fatal(`Could not load env variables from ${dotenvPath}`)
+    fatal(err)
   }
+}
 
-  loadEnv() {
-    try {
-      this.serverless.cli.log('DOTENV: Loading environment variables:');
-      const stage = this.serverless.processedInput.options.stage
-      const dotenvPath = stage ? `.env.${stage}` : '.env';
-      const envVariables = dotenvExpand(dotenv.config({ path: path.resolve(process.cwd(), dotenvPath) })).parsed;
-      if (!envVariables) {
-        throw new this.serverless.classes.Error('[serverless-dotenv-plugin] Could not find .env file.');
-        return false;
-      }
-
-      Object.keys(this.env)
-        .forEach((key) => {
-          this.serverless.cli.log("\t - " + key);
-          this.serverless.service.provider.environment[key] = env[key];
-        })
-    } catch (e) {
-        console.error('\n Serverless Plugin Error --------------------------------------\n')
-        console.error(`   ${e.message}`);
-    }
+class ServerlessPlugin {
+  constructor(serverless) {
+    loadEnv(serverless.processedInput.options.stage)
   }
 }
 
